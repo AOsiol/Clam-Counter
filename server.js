8916a8845b2a914aed36a8197542ee71210505aa
Anthony Osiol <funnynoun@gmail.com>
2020-06-13 21:52:27 -0400

    initial files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// this is hosted on heroku right now and should be directed there?
+// issues with installing mongoose  Also, how to tell if everything installed correctly?
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
