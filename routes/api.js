14acef153bb108d3bf028b9e896086af8401faa8
Anthony Osiol <funnynoun@gmail.com>
2020-06-19 22:59:04 -0400

    update db

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
-    .then((clams) => {
-      res.json(clams);
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
     })
     .catch((err) => {
       res.status(404).json(err);
@@ -13,8 +13,8 @@ router.post("/api/transaction", ({ body }, res) => {
 
 router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then((clams) => {
-      res.json(clams);
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
     })
     .catch((err) => {
       res.status(404).json(err);
@@ -24,8 +24,8 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
-    .then((clams) => {
-      res.json(clams);
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
     })
     .catch((err) => {
       res.status(404).json(err);
