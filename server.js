7cb17b56427f9d49233ea6d41e76c1cac4beebd0
Anthony Osiol <funnynoun@gmail.com>
2020-06-21 18:03:28 -0400

    database route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/clams";
 const PORT = 3000;
 
 const app = express();
@@ -24,8 +24,9 @@ app.use(express.static("public"));
 // issues with installing mongoose  Also, how to tell if everything installed correctly?
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://clams:5clams@ds253408.mlab.com:53408/heroku_5j8zjng2",
+  MONGODB_URI,
+  // process.env.MONGODB_URI ||
+  //   "mongodb://clams:5clams@ds253408.mlab.com:53408/heroku_5j8zjng2",
   {
     useNewUrlParser: true,
     useFindAndModify: false,
